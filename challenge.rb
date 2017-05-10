292cb7889cdd3ba006e0243af04eb1d0610ea7cd
Sam Morgan <s_morgan@me.com>
2017-05-10 09:56:33 +0100

    Add solution to Challenge 2 (1)

--- challenge.rb
+++ challenge.rb
@@ -7,6 +7,10 @@ def sort_numbers(array)
   array.sort
 end
 
+def increment_values_by_one(hash)
+  hash.transform_values { |value| value + 1 }
+end
+
 # TEST FRAMEWORK IMPLEMENTATION
 def assert_equal(expected_result, result)
   p "Expected: #{expected_result}, got: #{result}"
@@ -15,3 +19,4 @@ end
 # TESTS
 assert_equal([2, 3, 4, 5, 6], increment_elements_by_one([1, 2, 3, 4, 5]))
 assert_equal([1, 2, 3, 4, 5], sort_numbers([1, 3, 5, 4, 2]))
+assert_equal({ a: 2, b: 3 },  increment_values_by_one({ a: 1, b: 2 }))
